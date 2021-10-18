a0986e4a404d0d6127c172e6df15433f49479618
Daniel G. Taylor <danielgtaylor@gmail.com>
2021-10-17 22:06:36 -0700

    feat: use CLI shorthand for `sdt` command input

--- cmd/sdt/main.go
+++ cmd/sdt/main.go
@@ -3,32 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 
 	"github.com/danielgtaylor/sdt"
-	"gopkg.in/yaml.v3"
+	"github.com/danielgtaylor/shorthand"
 )
 
-func mustLoad(filename string, value interface{}) {
-	b, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := yaml.Unmarshal(b, value); err != nil {
-		panic(err)
-	}
-}
-
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: sdt doc.yaml <params.yaml")
+		os.Exit(1)
+	}
+
 	doc, err := sdt.NewFromFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
-	var params map[string]interface{}
-	mustLoad(os.Args[2], &params)
+	args := []string{}
+	if len(os.Args) > 2 {
+		args = os.Args[2:]
+	}
+	params, err := shorthand.GetInput(args)
+	if err != nil {
+		panic(err)
+	}
+	if params == nil {
+		params = map[string]interface{}{}
+	}
 
 	// Validate template output format
 	if errs := doc.ValidateTemplate(os.Args[1]); len(errs) > 0 {
@@ -40,7 +42,7 @@ func main() {
 	}
 
 	// Validate params from bp.Schema
-	if err := doc.ValidateInput(os.Args[2], params); err != nil {
+	if err := doc.ValidateInput("args", params); err != nil {
 		fmt.Println("Error while validating input params:")
 		fmt.Println(err)
 		os.Exit(1)
@@ -57,7 +59,7 @@ func main() {
 	}
 
 	// Confirm that the output conforms to the schema now that it's rendered.
-	if err := doc.ValidateOutput(os.Args[2], rendered); err != nil {
+	if err := doc.ValidateOutput(os.Args[1], rendered); err != nil {
 		fmt.Println("Error validating rendered output:")
 		fmt.Println(err)
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -4,10 +4,8 @@ go 1.16
 
 require (
 	github.com/antonmedv/expr v1.9.0
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/danielgtaylor/shorthand v1.0.0
 	github.com/santhosh-tekuri/jsonschema/v5 v5.0.0
 	github.com/stretchr/testify v1.7.0
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
